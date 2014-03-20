f9539a8aedaf509f0b647f4045fca08871c5ce30
Arseniy Trushin <142857@mail.ru>
2014-03-20 14:46:14 +0400

    cimg save image demo

---
+++ tass/part1/lect04/serp_cimg.cpp
@@ -0,0 +1,28 @@
+#include "CImg.h"
+using namespace cimg_library;
+int main() {
+  int n = 512;
+
+  CImg<unsigned char> image(n, n, 1, 3, 0);
+  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
+
+  for(int p = 0; p < n; ++p) {
+    for(int q = 0; q < n; ++q) {
+      int val = 0;
+      if ((p & q) == 0) {
+	val = 255;
+      }
+      image.atXY(q, p, 0, 0) = val;
+      image.atXY(q, p, 0, 1) = val;
+    }
+  }
+
+  CImgDisplay main_disp(image,"Click a point");
+  while (!main_disp.is_closed()) {
+    main_disp.wait();
+  }
+
+  image.save("serp.png");
+
+  return 0;
+}
