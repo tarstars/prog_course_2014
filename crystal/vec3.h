41c39e770f5ac14446da74f8e094bc8f5b49aec1
Arseniy Trushin <142857@mail.ru>
2014-04-10 15:07:52 +0400

    start of crystal project

---
+++ crystal/vec3.h
@@ -0,0 +1,19 @@
+
+
+class Vec3 {
+ public:
+  double x, y, z;
+};
+
+
+
+
+
+
+
+
+
+
+
+  //Vec3(double xx, double yy, double zz) : x(xx), y(yy), z(zz) {}
+
---
+++ crystal/vec3_test.cpp
@@ -0,0 +1,11 @@
+#include "vec3.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  Vec3 a;
+
+  a.x = 5;
+}
