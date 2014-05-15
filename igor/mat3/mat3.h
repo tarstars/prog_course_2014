9fd0e73617003ad2597ffe00a43ad2560b61cd4a
Igor <a@b>
2014-05-15 11:29:20 +0400

    matrix

---
+++ igor/mat3/mat3.h
@@ -0,0 +1,24 @@
+#ifndef MAT3_H_INCLUDED
+#define MAT3_H_INCLUDED
+
+#include <iostream>
+
+class Mat3
+{
+    double mat[3][3];
+public:
+    Mat3();
+    Mat3(const Mat3&);
+    double At(int, int);
+    void Set(int, int, double);
+    void T();
+
+    friend std::ostream& operator<<(std::ostream&, const Mat3&);
+    friend const Mat3 operator+(const Mat3&, const Mat3&);
+    friend const Mat3 operator-(const Mat3&, const Mat3&);
+    friend const Mat3 operator*(const Mat3&, const Mat3&);
+   // friend const Mat3 operator-(const Mat3&, const Mat3&);
+};
+
+
+#endif // MAT3_H_INCLUDED
