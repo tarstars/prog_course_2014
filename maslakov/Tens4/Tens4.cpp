784dc1b053d9e055e2c5c5b4cac397269a27a1fa
Alex <alex@aa.com>
2014-05-22 11:10:15 +0400

    material tensor update

--- maslakov/Tens4/Tens4.cpp
+++ maslakov/Tens4/Tens4.cpp
@@ -5,16 +5,7 @@
 
 using namespace std;
 
-Tens4::Tens4()
-{
-  int p,q,m,n;
-  
-  for(p = 0; p < 3; ++p)
-    for(q = 0; q < 3; ++q)
-      for(m = 0; m < 3; ++m)
-	for(n = 0; n < 3; ++n)
-	  dat[p][q][m][n] = 0;
-}
+
 
 Tens4::Tens4(const Tens4& t){
 int p,q,m,n;
@@ -27,6 +18,31 @@ int p,q,m,n;
 	  dat[p][q][m][n]=t.dat[p][q][m][n];
 }
 
+Tens4::Tens4(const Coeff& r){
+  int p,q,m,n;
+
+  for(p = 0; p < 3; ++p)
+    for(q = 0; q < 3; ++q)
+      for(m = 0; m < 3; ++m)
+	for(n = 0; n < 3; ++n)
+	  
+	  dat[p][q][m][n]=0;
+   dat[0][0][0][0] = dat[1][1][1][1] = r.c11;
+
+  dat[0][0][1][1] = dat[1][1][0][0] = r.c12;
+
+  dat[0][0][2][2] = dat[1][1][2][2] = dat[2][2][0][0] = dat[2][2][1][1] = r.c13;
+
+  dat[2][2][2][2] = r.c33;
+
+  dat[1][2][1][2] = dat[1][2][2][1] = dat[2][1][1][2] = dat[2][1][2][1] = 
+    dat[0][2][0][2] = dat[0][2][2][0] = dat[2][0][0][2] = dat[2][0][2][0] = r.c44;
+
+  dat[0][1][1][0] = dat[0][1][0][1] = dat[1][0][0][1] = dat[1][0][1][0] = r.c66;
+}
+  
+
+  
 
 float At(Tens4& t, int p,int q, int m, int n)
 {
--- maslakov/Tens4/main.cpp
+++ maslakov/Tens4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Tens4.h"
+
 using namespace std;
 
 
@@ -7,17 +8,11 @@ int main(){
 
  
  
-  Tens4 a;
+  Tens4 a(Coeff::paratellurite());
   cout<<a.dat[0][0][0][0]<<endl;
   
-  Tens4 b(a);
-  cout<<b.dat[0][0][0][0]<<endl;
-
-  Set(a,0,0,0,0,22);
-  cout<<a.dat[0][0][0][0]<<endl;
-  cout<<At(a,0,0,0,0)<<endl;
-
-  PrintTensor(a);
+ 
+  PrintTensor(a);//print tensor as linear array
   
   return 0;
 
